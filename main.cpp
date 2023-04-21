6c19e7b6a6a2ca67e86be2ad95fef176e2a1b847
Patrycioss <72610925+Patrycioss@users.noreply.github.com>
2023-04-21 23:43:52 +0200

    Made some tests to measure lua calling cpp function

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "lua_help.hpp"
 #include "tests/PalindromeTest.hpp"
 #include "tests/ObjectTest.hpp"
+#include "tests/CPP_FunctionTest.hpp"
 
 
 int main() {
@@ -17,11 +18,11 @@ int main() {
 	//Add 2 because will remove first and last result because better for tests
 	count += 2;
 
-	ObjectTest test(lua);
+	CPP_FunctionTest test(lua);
 	std::chrono::nanoseconds lua_total{};
 	std::chrono::nanoseconds cpp_total{};
 	
-	int max = (10);
+	int max = (1000);
 	
 	for (int i = 0; i < count; i++)
 	{
@@ -46,45 +47,6 @@ int main() {
 	std::cout << "CPP average: " << cpp_average.count() << " ns --- LUA average: " << lua_average.count() << " ns" << std::endl; 
 	std::cout << "CPP total: " << cpp_total.count() << " ns --- LUA total: " << lua_total.count() << " ns" << std::endl;
 	
-//	const std::function lua_test = lua_test_calculation2;
-//	const std::function cpp_test = cpp_test_calculation2;
-//
-//
-//
-
-
-//	NanoTimer lua_timer;
-//	NanoTimer cpp_timer;
-//
-//	for (int i = 0; i < count; i++)
-//	{
-//		lua_timer.Start();
-//		const auto lua_val = lua_test(lua);
-//		const std::chrono::nanoseconds lua_time = lua_timer.Stop();
-//		lua_pop(lua, lua_gettop(lua));
-//
-//		cpp_timer.Start();
-//		const auto cpp_val = cpp_test();
-//		const auto cpp_time = cpp_timer.Stop();
-//
-//
-//
-//		if (i != 0 && i != count - 1)
-//		{
-//			lua_total += lua_time;
-//			cpp_total += cpp_time;
-//
-//			std::cout << "LUA: " << lua_time << ", CPP: " << cpp_time << std::endl;
-//		}
-//
-//	}
-//
-//	const auto average_lua = lua_total / (count-2);
-//	const auto average_cpp = cpp_total / (count - 2);
-//
-//
-//	std::cout << "Lua average: " << average_lua << " nanoseconds" << std::endl;
-//	std::cout << "CPP average: " << average_cpp << " nanoseconds" << std::endl;
 
 	lua_close(lua);
 	return 0;
